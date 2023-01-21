d5253d54901c1ca7ed2d348371293353c7ba2d12
shubham lahoti <lahotishubham03@gmail.com>
2023-01-21 22:40:10 +0530

    Update Careers.jsx

--- src/pages/careers/Careers.jsx
+++ src/pages/careers/Careers.jsx
@@ -3,9 +3,9 @@ import { Link, useLoaderData } from 'react-router-dom';
 export default function Careers() {
   const careers = useLoaderData();
   return (
-    <div className='careers'>
-      {careers.map((career) => (
-        <Link to='/' key={career.id}>
+    <div className="careers">
+      {careers.map(career => (
+        <Link to={career.id.toString()} key={career.id}>
           <p>{career.title}</p>
           <p>Based in {career.location}</p>
         </Link>
